443c347d5dca986452617831ad27abfef4156ed8
Bikesh Kumar <iambikeshkumar07@gmail.com>
2026-04-14 23:47:30 +0530

    feat(auth): update signup and signin endpoints to use email instead of username; add password hashing and error handling feat(db): add dotenv dependency for environment variable management; update Prisma client import refactor(common): modify user schema to replace username with email for user creation and signin

--- draw-app/apps/backend/src/index.ts
+++ draw-app/apps/backend/src/index.ts
@@ -3,52 +3,76 @@ import  Jwt  from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
 import { CreateRoomSchema, CreateUserSchema, SigninSchema } from "@repo/common/types"
-const app = express();
 import { prismaclient } from "@repo/db/client";
+import bcrypt from "bcrypt-ts";
 
+const app = express();
 app.use(express.json());
 
 app.post("/signup", async (req,res) =>{
-    //db call 
     const parsedData = CreateUserSchema.safeParse(req.body);
     if(!parsedData.success){
+        console.log(parsedData.error);
         res.json({
             message:"Incoorect Inputs"
         })
         return;
     }
+    const hashedPassword = await bcrypt.hash(parsedData.data.password, 10);
     try{
        const user =  await prismaclient().user.create({
         data:{
-            email:parsedData.data?.username,
-            password: parsedData.data.password,
+            email:parsedData.data.email,
+            //hashed pasword
+            password: hashedPassword,
             name:parsedData.data.name
         }
     })
     res.json({
         userId: user.id
     })
-    } catch(e){
-        res.status(411).json({
-            message:"User Already exists with this username"
-        })
-    }
+    } catch (e: any) {
+    return res.status(500).json({
+        message: "Internal server error"
+    });
+}
     
 })
 
-app.post("/signin",(req,res)=>{
-    const data = SigninSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/signin", async (req,res)=>{
+    const parsedData = SigninSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.json({
             message:"Incoorect Inputs"
         })
         return;
     }
 
+    const user = await prismaclient().user.findUnique({
+        where:{
+            email:parsedData.data.email,
+        }
+    })
 
-    const userId = 1;
+    if(!user){
+        res.status(403).json({
+            message:"Not authorized"
+        })
+        return;
+    }
+    const isPasswordValid = await bcrypt.compare(
+        parsedData.data.password,
+        user.password
+    );
+    if (!isPasswordValid) {
+        return res.status(403).json({
+            message: "Not authorized"
+        });
+    }
+
+    
     const token = Jwt.sign({
-        userId
+        userId:user?.id
     },JWT_SECRET);
 
     res.json({
@@ -56,18 +80,33 @@ app.post("/signin",(req,res)=>{
     })
 })
 
-app.post("/room",middleware,(req,res)=>{
-    const data = CreateRoomSchema.safeParse(req.body);
-    if(!data.success){
+app.post("/room",middleware, async (req,res)=>{
+    const parsedData = CreateRoomSchema.safeParse(req.body);
+    if(!parsedData.success){
         res.json({
             message:"Incoorect Inputs"
         })
         return;
     }
+    //@ts-ignore
+    const userId = req.userId;
 
-    res.json({
-        roomId:123
+    try{
+    const room = await prismaclient().room.create({
+        data:{
+            slug:parsedData.data.roomname,
+            adminId:userId
+        }
     })
+    res.json({
+        roomId:room.id
+    })
+    }
+    catch(e){
+        res.status(411).json({
+            message:"Room already exists with this name"
+        })
+    }
 })
 
 app.listen(3001);
